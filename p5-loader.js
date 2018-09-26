fe1061c65a43e8d65856269d8f48186c7806d5fe
Joshua Castor <jcasto3@uic.edu>
2018-09-26 08:38:10 -0500

    Added p5 sound support to p5 loader

--- p5-loader.js
+++ p5-loader.js
@@ -13,6 +13,7 @@
     const mappings = {
       p5: ['https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.1/p5.min.js', '../processing-3.4/p5.min.js'],
       p5Dom: ['https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.1/addons/p5.dom.js', '../processing-3.4/p5-addons/p5.dom.js'],
+      p5Sound: ['https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.1/addons/p5.sound.js', '../processing-3.4/p5-addons/p5.sound.js'],
     }
 
     function loadScript ([main, alt]) {
@@ -26,6 +27,7 @@
     const p5Loader = {
       loadP5: () => loadScript(mappings.p5),
       loadP5Dom: () => loadScript(mappings.p5Dom),
+      loadP5Sound: () => loadScript(mappings.p5Sound),
     }
     
     window.p5Loader = p5Loader;
