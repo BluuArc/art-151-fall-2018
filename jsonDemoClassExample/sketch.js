d04c620dd7177fde5559f3a3b2001ab253c0d1c3
Joshua Castor <jcasto3@uic.edu>
2018-10-08 09:18:46 -0500

    Added more content to json demo example

--- jsonDemoClassExample/sketch.js
+++ jsonDemoClassExample/sketch.js
@@ -10,13 +10,24 @@ function App (_p5) {
 
   _p5.setup = () => {
     console.debug(myInfo);
-    _p5.createCanvas(_p5.windowWidth, _p5.windowHeight);
+    _p5.createCanvas(400, 400);
   };
 
   _p5.draw = () => {
     _p5.background(0);
-
     _p5.fill(255);
-    _p5.text(`welcome ${myInfo.name}`, _p5.width / 2, _p5.height / 2);
+    _p5.strokeWeight(0);
+    _p5.text(`welcome ${myInfo.name}`, 10, 30);
+    _p5.text(`Class Rank: ${myInfo.classRank}`, 10, 50);
+
+    const diffRange = 5;
+    const ageMap = _p5.map(myInfo.age, 0, 100, 0 + diffRange, 255 - diffRange);
+    const lineColor = ageMap + ((Math.random() * diffRange * 2) - diffRange);
+    _p5.stroke(lineColor);
+    _p5.strokeWeight(myInfo.age);
+    _p5.line(0, _p5.height / 2, _p5.width, _p5.height / 2);
+    _p5.fill(255 - lineColor);
+    _p5.strokeWeight(0);
+    _p5.text(myInfo.website, 10, _p5.height / 2);
   };
 }
